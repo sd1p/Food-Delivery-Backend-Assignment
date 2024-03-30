f4bb472c98e27ba4fc018684a9b36440698550ca
Sudip Mandal <skmxyt@gmail.com>
2024-03-30 23:50:33 +0530

    added more tests for api/delivery-price

--- src/tests/api/deliveryRoutes.test.ts
+++ src/tests/api/deliveryRoutes.test.ts
@@ -15,6 +15,7 @@ describe('POST /api/delivery-price', () => {
   })
 
   // error handling
+
   it('should return 400 if organizationId is missing', async () => {
     const response = await request(app).post('/api/delivery-price').send({
       zone: 'central',
@@ -29,6 +30,96 @@ describe('POST /api/delivery-price', () => {
         expected: 'number',
         received: 'undefined',
         path: ['organizationId'],
+        message: 'Required',
+      },
+    ])
+  })
+
+  it('should return 400 if itemType is invalid', async () => {
+    const response = await request(app).post('/api/delivery-price').send({
+      organizationId: 2,
+      zone: 'central',
+      totalDistance: 12,
+      itemType: 'test-123',
+    })
+
+    expect(response.status).toBe(400)
+    expect(JSON.parse(response.body.error)).toEqual([
+      {
+        code: 'invalid_enum_value',
+        message:
+          "Invalid enum value. Expected 'perishable' | 'non-perishable', received 'test-123'",
+        options: ['perishable', 'non-perishable'],
+        path: ['itemType'],
+        received: 'test-123',
+      },
+    ])
+  })
+
+  it('should return 404 when no pricing is available for the given zone', async () => {
+    const response = await request(app).post('/api/delivery-price').send({
+      organizationId: 2,
+      zone: 'test-123',
+      totalDistance: 12,
+      itemType: 'perishable',
+    })
+
+    expect(response.status).toBe(404)
+    expect(response.body).toEqual({ error: 'Pricing not found' })
+  })
+
+  it('should return 400 if zone is missing', async () => {
+    const response = await request(app).post('/api/delivery-price').send({
+      organizationId: 2,
+      totalDistance: 12,
+      itemType: 'perishable',
+    })
+
+    expect(response.status).toBe(400)
+    expect(JSON.parse(response.body.error)).toEqual([
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        received: 'undefined',
+        path: ['zone'],
+        message: 'Required',
+      },
+    ])
+  })
+
+  it('should return 400 if totalDistance is missing', async () => {
+    const response = await request(app).post('/api/delivery-price').send({
+      organizationId: 2,
+      zone: 'central',
+      itemType: 'perishable',
+    })
+
+    expect(response.status).toBe(400)
+    expect(JSON.parse(response.body.error)).toEqual([
+      {
+        code: 'invalid_type',
+        expected: 'number',
+        received: 'undefined',
+        path: ['totalDistance'],
+        message: 'Required',
+      },
+    ])
+  })
+
+  it('should return 400 if itemType is missing', async () => {
+    const response = await request(app).post('/api/delivery-price').send({
+      organizationId: 2,
+      zone: 'central',
+      totalDistance: 12,
+    })
+
+    expect(response.status).toBe(400)
+    expect(JSON.parse(response.body.error)).toEqual([
+      {
+        code: 'invalid_type',
+        expected: "'perishable' | 'non-perishable'",
+        received: 'undefined',
+        path: ['itemType'],
         message: 'Required',
       },
     ])
